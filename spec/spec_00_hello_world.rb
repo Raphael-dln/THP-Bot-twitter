42b68d38f0d7e80d40d40c82692b0bcbb15fd874
Raphael-dln <delannay.raphael@gmail.com>
2020-04-15 16:07:02 +0200

    exercises 1 & 2

--- spec/spec_00_hello_world.rb
+++ spec/spec_00_hello_world.rb
@@ -1,1 +1,7 @@
 require_relative '../lib/00_hello_world'
+
+describe "the login_twitter method" do
+  it "should return client, and client is not nil" do
+    expect(login_twitter).not_to be_nil
+  end
+end
